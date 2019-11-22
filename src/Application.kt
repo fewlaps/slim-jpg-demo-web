c92c7eae41ed60a2f54a9ab908e21742fce7be15
Roc Boronat <roc@fewlaps.com>
2019-11-22 09:52:16 +0100

    Add a form to send the picture to the /optimize post method

--- src/Application.kt
+++ src/Application.kt
@@ -1,14 +1,25 @@
 package com.fewlaps.slimjpg
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.html.*
-import kotlinx.html.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.features.*
-import org.slf4j.event.*
+import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.PartData
+import io.ktor.http.content.forEachPart
+import io.ktor.http.content.streamProvider
+import io.ktor.request.path
+import io.ktor.request.receiveMultipart
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.util.hex
+import kotlinx.html.*
+import kotlinx.io.core.readBytes
+import org.slf4j.event.Level
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -38,20 +49,43 @@ fun Application.module(testing: Boolean = false) {
 
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        get("/html-dsl") {
             call.respondHtml {
                 body {
-                    h1 { +"HTML" }
-                    ul {
-                        for (n in 1..10) {
-                            li { +"$n" }
+                    h1 { +"Welcome to the Slim JPG demo page!" }
+                    p { +"Upload a picture and it will be optimized." }
+                    form("/optimize", encType = FormEncType.multipartFormData, method = FormMethod.post) {
+                        p {
+                            label { +"Picture: " }
+                            fileInput { name = "picture" }
+                        }
+                        p {
+                            submitInput { value = "Optimize" }
                         }
                     }
                 }
             }
+        }
+
+        post("/optimize") {
+            val multipart = call.receiveMultipart()
+            val out = arrayListOf<String>()
+            multipart.forEachPart { part ->
+                out += when (part) {
+                    is PartData.FormItem -> {
+                        "FormItem(${part.name},${part.value})"
+                    }
+                    is PartData.FileItem -> {
+                        val bytes = part.streamProvider().readBytes()
+                        "FileItem(${part.name},${part.originalFileName},${hex(bytes)})"
+                    }
+                    is PartData.BinaryItem -> {
+                        "BinaryItem(${part.name},${hex(part.provider().readBytes())})"
+                    }
+                }
+
+                part.dispose()
+            }
+            call.respondText(out.joinToString("; "))
         }
 
         install(StatusPages) {
